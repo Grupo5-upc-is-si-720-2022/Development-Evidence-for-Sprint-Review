cf760108b6cf4adc16d7fb43c2aa5080cc4255ad
QuiqueVladimir <U202022365@upc.edu.pe>
2022-11-24 02:22:57 -0500

    ListSession and Patient

--- src/main/java/pe/edu/dps/Sessions/Date.java
+++ src/main/java/pe/edu/dps/Sessions/Date.java
@@ -1,4 +1,17 @@
 package pe.edu.dps.Sessions;
 
 public class Date {
+    private int month;
+    private int day;
+    private int year;
+
+    public void monthToString(){
+
+    }
+    public void dayToString(){
+
+    }
+    public void yearToString(){
+
+    }
 }

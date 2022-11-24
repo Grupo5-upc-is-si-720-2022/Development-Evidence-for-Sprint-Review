fc92a30018c0a8884f24d247ddbbbcedeac87a00
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:37:49 -0500

    src(SubscriptionPremium) Update Getters and Setters

--- src/main/java/pe/edu/dps/Subscription/SubscriptionPremium.java
+++ src/main/java/pe/edu/dps/Subscription/SubscriptionPremium.java
@@ -1,4 +1,22 @@
 package pe.edu.dps.Subscription;
-
+import pe.edu.dps.Sessions.Date;
 public class SubscriptionPremium {
+    private Date startDate;
+    private Date finalDate;
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(Date finalDate) {
+        this.finalDate = finalDate;
+    }
 }

07313a86533463ef48c72c362166157a517cd002
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:19:41 -0500

    src(Qualification) Create Getters and Setters

--- src/main/java/pe/edu/dps/Users/ElementTherapist/Qualification.java
+++ src/main/java/pe/edu/dps/Users/ElementTherapist/Qualification.java
@@ -3,5 +3,11 @@ package pe.edu.dps.Users.ElementTherapist;
 public class Qualification {
     private int amount;
 
+    public int getAmount() {
+        return amount;
+    }
 
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }

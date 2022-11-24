ab1558356d592c4f194bd942f679607e8678a5d8
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:39:18 -0500

    src(Session) Create Getters and Setters

--- src/main/java/pe/edu/dps/Sessions/Session.java
+++ src/main/java/pe/edu/dps/Sessions/Session.java
@@ -6,4 +6,36 @@ public class Session {
     private Schedule schedule;
     private String report;
     private boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }

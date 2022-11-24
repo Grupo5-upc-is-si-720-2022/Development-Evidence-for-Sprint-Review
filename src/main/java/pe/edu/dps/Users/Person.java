049d8646c034c9df023c298c3f9436334f9f915d
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:15:57 -0500

    src(Person) Create Getters and Setters

--- src/main/java/pe/edu/dps/Users/Person.java
+++ src/main/java/pe/edu/dps/Users/Person.java
@@ -9,4 +9,52 @@ public abstract class Person {
     private int uid;
     private String password;
     private Inbox messengerService;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Inbox getMessengerService() {
+        return messengerService;
+    }
+
+    public void setMessengerService(Inbox messengerService) {
+        this.messengerService = messengerService;
+    }
 }

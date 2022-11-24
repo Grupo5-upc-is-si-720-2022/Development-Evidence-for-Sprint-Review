7fde65e9d9d472b2932c56f6c5f5634872c85803
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:46:52 -0500

    src(Message) Create Getters and Setters

--- src/main/java/pe/edu/dps/Chats/Message.java
+++ src/main/java/pe/edu/dps/Chats/Message.java
@@ -1,4 +1,52 @@
 package pe.edu.dps.Chats;
+import pe.edu.dps.Chats.File;
+import java.util.List;
+import pe.edu.dps.Sessions.Date;
 
 public class Message {
+    private int id;
+    private String content;
+    private List<File> attachedFiles;
+    private Date dateSend;
+    private Date dateReceived;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<File> getAttachedFiles() {
+        return attachedFiles;
+    }
+
+    public void setAttachedFiles(List<File> attachedFiles) {
+        this.attachedFiles = attachedFiles;
+    }
+
+    public Date getDateSend() {
+        return dateSend;
+    }
+
+    public void setDateSend(Date dateSend) {
+        this.dateSend = dateSend;
+    }
+
+    public Date getDateReceived() {
+        return dateReceived;
+    }
+
+    public void setDateReceived(Date dateReceived) {
+        this.dateReceived = dateReceived;
+    }
 }

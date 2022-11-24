12f361a1528fc9ec211464140f1a20ddecd9f634
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:41:19 -0500

    src(Date) Create Getters and Setters

--- src/main/java/pe/edu/dps/Sessions/Date.java
+++ src/main/java/pe/edu/dps/Sessions/Date.java
@@ -14,4 +14,28 @@ public class Date {
     public void yearToString(){
 
     }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }

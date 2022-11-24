52d50364a26d133899c3dad06621eb301a3dc861
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:14:14 -0500

    src(therapist) Create Getters and Setters

--- src/main/java/pe/edu/dps/Users/Therapist.java
+++ src/main/java/pe/edu/dps/Users/Therapist.java
@@ -10,6 +10,7 @@ import pe.edu.dps.Chats.Inbox;
 import java.util.List;
 
 public class Therapist extends Person {
+
     private BachelorDegree degree;
     private List<Patient> patients;
     private List<Schedule> scheduleAvalible;
@@ -20,4 +21,52 @@ public class Therapist extends Person {
     public void addSchedule(){
 
     }
+    public BachelorDegree getDegree() {
+        return degree;
+    }
+
+    public void setDegree(BachelorDegree degree) {
+        this.degree = degree;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
+
+    public List<Schedule> getScheduleAvalible() {
+        return scheduleAvalible;
+    }
+
+    public void setScheduleAvalible(List<Schedule> scheduleAvalible) {
+        this.scheduleAvalible = scheduleAvalible;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public Inbox getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(Inbox inbox) {
+        this.inbox = inbox;
+    }
+
+    public AmountPayment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(AmountPayment payment) {
+        this.payment = payment;
+    }
+
 }

a579de7c8bbaa11394f16f5218a6e8da3f7772a3
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:41:47 -0500

    src(Text) Create Getters and Setters

--- src/main/java/pe/edu/dps/Exercise/Text.java
+++ src/main/java/pe/edu/dps/Exercise/Text.java
@@ -4,4 +4,19 @@ public class Text {
     private int Id;
     private String reader;
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public void setReader(String reader) {
+        this.reader = reader;
+    }
 }

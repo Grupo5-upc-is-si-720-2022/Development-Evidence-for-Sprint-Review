cba8811d7f386d410c2ba5335a5387942ef192d2
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:50:38 -0500

    src(Document) Create Getters and Setters

--- src/main/java/pe/edu/dps/Chats/Document.java
+++ src/main/java/pe/edu/dps/Chats/Document.java
@@ -1,4 +1,13 @@
 package pe.edu.dps.Chats;
 
 public class Document {
+    private int numberPages;
+
+    public int getNumberPages() {
+        return numberPages;
+    }
+
+    public void setNumberPages(int numberPages) {
+        this.numberPages = numberPages;
+    }
 }

14e63e049479914c8a05b39fa9530c4ea3589621
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:34:29 -0500

    src(Parent) Update Getters and Setters

--- src/main/java/pe/edu/dps/Users/Parent.java
+++ src/main/java/pe/edu/dps/Users/Parent.java
@@ -1,27 +1,67 @@
 package pe.edu.dps.Users;
-
-<<<<<<< HEAD
-import
-public class Parent
-{
-    private int dni;
-    private String celNumber;
-
-=======
 import pe.edu.dps.Users.ElementTherapist.Qualification;
 import pe.edu.dps.Users.Patient;
 import pe.edu.dps.Subscription.Subscription;
+import pe.edu.dps.Chats.Inbox;
 import java.util.List;
 
 public class Parent extends Person {
     private int dni;
     private String celNumer;
+    private Inbox inbox;
     private List<Qualification> qualifiedTerapist;
     private List<Patient> tutored;
     private Subscription subscription;
 
-    public void addTutored(Patient patient){
+    public void addTutored(Patient patient) {
 
     }
->>>>>>> 2297912c9ad41b2be42b3677852c3b8bf04473e4
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getCelNumer() {
+        return celNumer;
+    }
+
+    public void setCelNumer(String celNumer) {
+        this.celNumer = celNumer;
+    }
+
+    public Inbox getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(Inbox inbox) {
+        this.inbox = inbox;
+    }
+
+    public List<Qualification> getQualifiedTerapist() {
+        return qualifiedTerapist;
+    }
+
+    public void setQualifiedTerapist(List<Qualification> qualifiedTerapist) {
+        this.qualifiedTerapist = qualifiedTerapist;
+    }
+
+    public List<Patient> getTutored() {
+        return tutored;
+    }
+
+    public void setTutored(List<Patient> tutored) {
+        this.tutored = tutored;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
 }

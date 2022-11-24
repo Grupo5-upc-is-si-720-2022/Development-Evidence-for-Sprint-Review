4ce72e1d34a82073475612a8f5253c0a5f1ce019
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:44:15 -0500

    src(Exercise) Create Getters and Setters

--- src/main/java/pe/edu/dps/Exercise/Exercise.java
+++ src/main/java/pe/edu/dps/Exercise/Exercise.java
@@ -9,5 +9,43 @@ public class Exercise {
     private int time;
     private float progress;
 
+    public String getId() {
+        return Id;
+    }
 
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getDificulty() {
+        return dificulty;
+    }
+
+    public void setDificulty(int dificulty) {
+        this.dificulty = dificulty;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public void setProgress(float progress) {
+        this.progress = progress;
+    }
 }

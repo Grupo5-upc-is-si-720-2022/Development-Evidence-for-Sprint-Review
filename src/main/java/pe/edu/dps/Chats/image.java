724c2e059f76c702837d6137b6f6edf5b2bbdbaf
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:48:20 -0500

    src(Image) Create Getters and Setters

--- src/main/java/pe/edu/dps/Chats/image.java
+++ src/main/java/pe/edu/dps/Chats/image.java
@@ -1,4 +1,22 @@
 package pe.edu.dps.Chats;
 
 public class image {
+    private String alt;
+    private String source;
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
 }

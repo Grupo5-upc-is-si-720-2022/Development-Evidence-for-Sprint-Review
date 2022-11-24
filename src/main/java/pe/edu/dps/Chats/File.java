dd2465173a861ae3938a809b0291f71575657231
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:50:04 -0500

    src(File) Create Getters and Setters

--- src/main/java/pe/edu/dps/Chats/File.java
+++ src/main/java/pe/edu/dps/Chats/File.java
@@ -1,4 +1,35 @@
 package pe.edu.dps.Chats;
 
 public class File {
+    private String extension;
+    private String name;
+    private float weightFile;
+
+    public void openFile(){
+
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeightFile() {
+        return weightFile;
+    }
+
+    public void setWeightFile(float weightFile) {
+        this.weightFile = weightFile;
+    }
 }

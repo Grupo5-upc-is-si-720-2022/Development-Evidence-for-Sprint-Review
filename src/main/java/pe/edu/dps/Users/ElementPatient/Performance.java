81707e9f5f5b46445487778f1004ce5036aad63d
QuiqueVladimir <U202022365@upc.edu.pe>
2022-11-24 01:52:18 -0500

    Users and subscription

---
+++ src/main/java/pe/edu/dps/Users/ElementPatient/Performance.java
@@ -0,0 +1,12 @@
+package pe.edu.dps.Users.ElementPatient;
+
+public class Performance {
+
+    private float reading;
+    private float comunication;
+    private float learning;
+
+    public void showPerformance(){
+
+    }
+}

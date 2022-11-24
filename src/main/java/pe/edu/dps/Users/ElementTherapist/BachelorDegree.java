4b9a95d0dc56ac91b2d593dc21e3fd8037971668
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:21:16 -0500

    src(BachelorDegree) Create Getters and Setters

--- src/main/java/pe/edu/dps/Users/ElementTherapist/BachelorDegree.java
+++ src/main/java/pe/edu/dps/Users/ElementTherapist/BachelorDegree.java
@@ -1,5 +1,22 @@
 package pe.edu.dps.Users.ElementTherapist;
 
 public class BachelorDegree {
+    private String dateIssue;
+    private String university;
 
+    public String getDateIssue() {
+        return dateIssue;
+    }
+
+    public void setDateIssue(String dateIssue) {
+        this.dateIssue = dateIssue;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
 }

9f96264949751033ac52424cf2bd306d5323d540
AndersonGamarraV <u202016154@upc.edu.pe>
2022-11-24 02:23:31 -0500

    src(AmountPayment) Create Getters and Setters

--- src/main/java/pe/edu/dps/Users/ElementTherapist/AmountPayment.java
+++ src/main/java/pe/edu/dps/Users/ElementTherapist/AmountPayment.java
@@ -1,4 +1,14 @@
 package pe.edu.dps.Users.ElementTherapist;
 
 public class AmountPayment {
+    private float total;
+    private float amountXSessions;
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
 }
